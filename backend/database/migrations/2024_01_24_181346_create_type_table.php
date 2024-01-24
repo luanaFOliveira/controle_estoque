cc1baae04d383cd912fd1529084e0a47fdb5790a
luanaFOliveira <luluoliveira7@gmail.com>
2024-01-24 15:16:01 -0300

    :sparkles: feat: Criacao tabela tipo

---
+++ backend/database/migrations/2024_01_24_181346_create_type_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('type', function (Blueprint $table) {
+            $table->id('type_id');
+            $table->text('name');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('type');
+    }
+}
