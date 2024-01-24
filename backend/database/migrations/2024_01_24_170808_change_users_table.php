66a6e53538415fb57db172a89dd734458ac2adf7
luanaFOliveira <luluoliveira7@gmail.com>
2024-01-24 14:37:27 -0300

    :sparkles: feat: migracao para alteracao da tabela user

---
+++ backend/database/migrations/2024_01_24_170808_change_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('id', 'user_id');
+            $table->boolean('is_admin')->default(false);
+        });
+
+        Schema::rename('users', 'user');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('user_id', 'id');
+            $table->dropColumn('is_admin');
+            $table->softDeletes();
+        });
+    }
+}
