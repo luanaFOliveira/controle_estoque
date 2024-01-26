344f59b573f93f0cf8c5366e9242ce08940f4aeb
luanaFOliveira <luluoliveira7@gmail.com>
2024-01-26 17:52:59 -0300

    :sparkles: feat: Teste de autenticacao

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
+Route::post('/logout',[AuthController::class, 'logout']);
+
+Route::get('/users',[UserController::class, 'index']);
+Route::get('/users/{user}',[UserController::class, 'show']);
+Route::put('/users/{user}',[UserController::class, 'update']);
+Route::delete('/users/{user}',[UserController::class, 'destroy']);
+
