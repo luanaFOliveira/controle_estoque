8346baf61e77ada3b48acf773cb76f52027d8774
Otávio Gonçalves <129676776+otaaaviio@users.noreply.github.com>
2024-01-29 15:01:30 -0300

    :test_tube: test: Equipamentos

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('equipments', \App\Http\Controllers\EquipmentController::class);
+});
---
+++ backend/tests/Feature/EquipmentTest.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Equipment;
+use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
+
+beforeEach(function (){
+    Artisan::call('migrate:refresh');
+    Artisan::call('db:seed');
+    $user = User::factory()->create();
+    $this->actingAs($user, 'sanctum');
+});
+
+uses()->group('equipment');
+
+test('can create a equipment', function () {
+    $data = [
+        'name' => 'test name',
+        'equipment_type' => 'test type',
+        'equipment_brand' => 'test brand',
+        'sector' =>  'test sector',
+        'is_available' => true,
+        'is_at_office' => true,
+    ];
+
+    $response = $this->postJson('/api/equipments', $data);
+    $response->assertCreated();
+    $equipmentId = $response->json('data.equipment_id');
+    $equipment = Equipment::find($equipmentId);
+
+    expect($equipment)->not()->toBeNull();
+});
+
+test('cannot create a equipment with invalid data', function () {
+    $data = [
+        'name' => '',
+    ];
+
+    $response = $this->postJson('/api/equipments', $data);
+    $response->assertStatus(422);
+    $response->assertJsonValidationErrors('name');
+});
+
+test('can update a equipment', function () {
+    /* @var Equipment $equipment
+     * */
+    $equipment = Equipment::factory()->create();
+    $updatedEquipment = [
+        'name' => 'new test name',
+        'sector' => 'new test sector',
+        'equipment_type' => 'new test type',
+        'equipment_brand' => 'new test brand',
+        'is_available' => false,
+        'is_at_office' => false,
+    ];
+
+    $equipment_id = $equipment->equipment_id;
+    $response = $this->putJson("/api/equipments/{$equipment_id}", $updatedEquipment);
+    $response->assertStatus(200);
+
+    $response->assertJson([
+        'data' => [
+            'equipment_id' => $equipment_id,
+            'name' => $updatedEquipment['name'],
+            'type' => $updatedEquipment['equipment_type'],
+            'brand' => $updatedEquipment['equipment_brand'],
+            'sector' => $updatedEquipment['sector'],
+            'is_available' => $updatedEquipment['is_available'],
+            'is_at_office' => $updatedEquipment['is_at_office']
+        ]
+    ]);
+});
+
+test('cannot update a equipment with invalid data', function () {
+    /* @var Equipment $equipment
+     * */
+    $equipment = Equipment::factory()->create();
+    $updatedEquipment = [
+        'name' => '',
+        'sector' => '',
+        'equipment_type' => '',
+        'equipment_brand' => '',
+        'is_available' => null,
+        'is_at_office' => null,
+    ];
+
+    $equipment_id = $equipment->equipment_id;
+    $response = $this->putJson("/api/equipments/{$equipment_id}", $updatedEquipment);
+    $response->assertStatus(422);
+});
+
+test('can delete a equipment', function () {
+    /* @var Equipment $equipment
+     * */
+    $equipment = Equipment::factory()->create();
+    $this->assertDatabaseHas('equipment', ['equipment_id' => $equipment->equipment_id]);
+
+    $equipment_id = $equipment->equipment_id;
+    $response = $this->deleteJson("/api/equipments/{$equipment_id}");
+    $response->assertStatus(200);
+
+    $this->assertSoftDeleted('equipment', ['equipment_id' => $equipment->equipment_id]);
+});
+
+test('should return a paginated list of equipments', function () {
+    $response = $this->getJson('/api/equipments');
+    $paginatedResponse = $response->json();
+
+    expect($paginatedResponse)->toBePaginated();
+
+    foreach ($paginatedResponse['data'] as $equipment) {
+        expect($equipment)->toHaveKeys([
+            'equipment_id',
+            'name',
+            'type',
+            'brand',
+            'sector',
+            'is_available',
+            'is_at_office'
+        ]);
+    }
+});
+
+test('should show a detailed equipment', function () {
+    /* @var Equipment $equipment
+     * */
+    $equipment = Equipment::factory()->create();
+    $equipment_id = $equipment->equipment_id;
+    $response = $this->getJson("/api/equipments/{$equipment_id}");
+
+    $response->assertJson([
+        'data' => [
+            'equipment_id' => $equipment->equipment_id,
+            'name' => $equipment->name,
+            'type' => $equipment->type()->value('name'),
+            'brand' => $equipment->brand()->value('name'),
+            'sector' => $equipment->sector()->value('name'),
+            'is_available' => $equipment->is_available,
+            'is_at_office' => $equipment->is_at_office
+        ]
+    ]);
+});
+
+test('cannot access non-existent equipment', function () {
+    $nonExistentId = 12345;
+
+    $response = $this->getJson("/api/equipments/{$nonExistentId}");
+    $response->assertStatus(404);
+});
