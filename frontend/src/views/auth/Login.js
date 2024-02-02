f15ee927ee12c5e8c9b4d12b8c15b171b5743b00
Otávio Gonçalves <129676776+otaaaviio@users.noreply.github.com>
2024-02-02 17:08:19 -0300

    :sparkles: feat: Login

--- frontend/src/views/auth/Login.js
+++ frontend/src/views/auth/Login.js
@@ -1,66 +1,92 @@
-import React ,{ useContext,useState }from 'react';
-import {useAuth } from '../../context/AuthProvider';
-import { ThemeContext } from '../../context/ThemeProvider';
-import ToggleTheme from '../../components/ToggleTheme';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-export default function Login(){
-    const { theme } = useContext(ThemeContext);
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 
-    const {signed,Login} = useAuth();
-    console.log(signed);
 
-    const [credentials,setCredentials] = useState({
-        email:'',
-        password:'',
-    });
+const defaultTheme = createTheme();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setCredentials((prevCredentials) => ({
-          ...prevCredentials,
-          [name]: value,
-        }));
+export default function Login() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
     };
 
-    async function handleSubmit(){
-        
-        await Login({
-            email:credentials.email,
-            password:credentials.password,
-        });
-        
-    }
-
-    return(
-        <div style={{backgroundColor:theme.background,color:theme.text}}>
-            <h1>Login</h1>
-            <ToggleTheme/>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email</label>
-                    <input
-                        type="text"
-                        id="email"
-                        name="email"
-                        value={credentials.email}
-                        onChange={handleChange}
-                        required
-                    />
-                    </div>
-                    <div>
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={credentials.password}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <button type='submit'>Login</button>
-            </form>
-        </div>
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        BEM VINDO
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            ENTRAR
+                        </Button>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+        </ThemeProvider>
     );
-
-};
+}
