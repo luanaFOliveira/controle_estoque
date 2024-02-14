e8524aa64092e1aa521d42434968d68e5e33c636
Angelo Lorensi <angelolorensi@gmail.com>
2024-02-14 15:29:47 -0300

    :bug: fix: Erro na tabela de histórico

--- frontend/src/components/columns/historyTableColumns.jsx
+++ frontend/src/components/columns/historyTableColumns.jsx
@@ -3,9 +3,10 @@ import { useNavigate } from "react-router-dom";
 export default function HistoryTableColumns() {
   return [
     {
-      field: "user",
-      headerName: "Usuário",
-      width: 70,
+      field: 'user',
+      headerName: 'ID do Usuário',
+      width: 170,
+      renderCell: (params) => params.value.user_id,
     },
     {
       field: "equipment_code",
