cf1fa68f0feb0ef58ad0dab8b8a906ac07d8c3ed
luanaFOliveira <luluoliveira7@gmail.com>
2024-02-15 17:21:26 -0300

    :bricks: ci: Refatoracao de codigo pagina de usuario

--- frontend/src/services/historyService.js
+++ frontend/src/services/historyService.js
@@ -1,6 +1,11 @@
 import { api } from "./api";
 
-export async function getUserHistory(user_id) {
-  const response = await api.get(`/history/users?user_id=${user_id}`);
+export async function getUserHistory({user_id,filter}) {
+  const response = await api.get(`/history/users`,{
+    params: {
+      user_id: user_id,
+      availability : filter,
+    }
+  });
   return response.data;
 }
