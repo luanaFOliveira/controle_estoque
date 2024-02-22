0b3653e16ed807e85c9ab38b3b8731c0793a95fb
Otávio Gonçalves <129676776+otaaaviio@users.noreply.github.com>
2024-02-22 20:54:28 -0300

    :sparkles: feat: tratamento de erros das requisições de requisiçao de equipamento

--- frontend/src/services/equipmentRequestService.js
+++ frontend/src/services/equipmentRequestService.js
@@ -1,30 +1,30 @@
-import { api } from "./api";
+import {api, handleResponse} from "./api";
 
 export async function indexEquipmentRequests({ filter, page }) {
   const response = await api.get(`/equipment-requests?page=${page}`, {
     params: filter,
   });
-  return response.data;
+  return handleResponse(response);
 }
 
 export async function createEquipmentRequests({ formData }) {
   const response = await api.post(`/equipment-requests`, formData);
-  return response.data;
+  return handleResponse(response);
 }
 
 export async function getRequestMotives() {
   const response = await api.get(`/request-motives`);
-  return response.data;
+  return handleResponse(response);
 }
 
 export async function returnEquipment({ equipment }) {
   const response = await api.post(`/equipment/return/${equipment}`);
-  return response.data;
+  return handleResponse(response);
 }
 
 export async function updateEquipmentRequest({ equipment_request_id, action }) {
   const response = await api.post(`/handle-equipment-request/${action}`, {
     equipment_request_id,
   });
-  return response.data;
+  return handleResponse(response);
 }
--- frontend/src/views/AdminPages/equipmentRequests/EquipmentRequests.jsx
+++ frontend/src/views/AdminPages/equipmentRequests/EquipmentRequests.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from "react";
-import {errorToast} from "../../../services/api";
 import {indexEquipmentRequests} from "../../../services/equipmentRequestService";
 import {Box, CircularProgress, Container} from "@mui/material";
 import Grid from "@mui/material/Grid";
@@ -35,9 +34,7 @@ const EquipmentRequests = () => {
     });
 
     const [filter, setFilter] = useState({
-        searchPending: "none",
-        searchProcessed: "none",
-        status: "Nao pendente",
+        searchPending: "none", searchProcessed: "none", status: "Nao pendente",
     });
 
     useEffect(() => {
@@ -50,40 +47,31 @@ const EquipmentRequests = () => {
 
     const getPendingRequests = async () => {
         setIsLoadingPending(true);
-        try {
-            const page = pendingPaginationModel.page + 1;
-            await indexEquipmentRequests({
-                filter: {status: "Pendente", search: filter.searchPending}, page: page,
-            }).then((res) => {
-                setPendingRequests(res.data);
-                setPendingRowCount((prevRowCountState) => res.meta.total ?? prevRowCountState,);
-            });
-        } catch (error) {
-            console.error(error);
-            errorToast(error);
-        } finally {
+        const page = pendingPaginationModel.page + 1;
+        const res = await indexEquipmentRequests({
+            filter: {status: "Pendente", search: filter.searchPending}, page: page,
+        }).finally(() => {
             setIsLoadingPending(false);
             setIFirstLoading(false);
+        })
+        if (res) {
+            setPendingRequests(res.data);
+            setPendingRowCount((prevRowCountState) => res.meta.total ?? prevRowCountState,);
         }
     };
 
     const getProcessedRequests = async () => {
         setIsLoadingProcessed(true);
-        try {
-            const page = processedPaginationModel.page + 1;
-            await indexEquipmentRequests({
-                filter: {status: filter.status, search: filter.searchProcessed},
-                page: page,
-            }).then((res) => {
-                setProcessedRequests(res.data);
-                setProcessedRowCount((prevRowCountState) => res.meta.total ?? prevRowCountState,);
-            });
-        } catch (error) {
-            console.error(error);
-            errorToast(error);
-        } finally {
+        const page = processedPaginationModel.page + 1;
+        const res = await indexEquipmentRequests({
+            filter: {status: filter.status, search: filter.searchProcessed}, page: page,
+        }).finally(() => {
             setIsLoadingProcessed(false);
             setIFirstLoading(false);
+        })
+        if (res) {
+            setProcessedRequests(res.data);
+            setProcessedRowCount((prevRowCountState) => res.meta.total ?? prevRowCountState);
         }
     };
 
@@ -94,22 +82,23 @@ const EquipmentRequests = () => {
     const handleSearchProcessed = (searchProcessed) => {
         setFilter((prevFilter) => ({...prevFilter, searchProcessed}));
     };
-    
+
     const handleAvailabilityChange = (status) => {
         let newStatus = "Nao pendente";
-        if(status === "all"){
+        if (status === "all") {
             newStatus = "Nao pendente";
-        }else{
+        } else {
             newStatus = status ? "Aprovado" : "Não Aprovado";
         }
-        setFilter((prevFilter) => ({ ...prevFilter, status: newStatus }));
+        setFilter((prevFilter) => ({...prevFilter, status: newStatus}));
     };
 
     return (<Container sx={{mt: 5}}>
         {firstLoading ? (<Grid item container justifyContent="center">
             <CircularProgress/>
         </Grid>) : (<Box sx={{width: '100%'}}>
-            <TableTab value={tabValue} setValue={setTabValue} nameTabs={["Solicitações pendentes","Solicitações processadas"]}/>
+            <TableTab value={tabValue} setValue={setTabValue}
+                      nameTabs={["Solicitações pendentes", "Solicitações processadas"]}/>
             <CustomTabPanel value={tabValue} index={0}>
                 <FilterBox onSearch={handleSearchPending} disponibility={false}
                            label='Pesquisar Código do equipamento ou nome do usuario'/>
