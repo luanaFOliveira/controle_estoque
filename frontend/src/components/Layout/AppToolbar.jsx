3b1c2954146be510611486b96fa0b6de2b4e4a88
Otávio Gonçalves <129676776+otaaaviio@users.noreply.github.com>
2024-02-22 21:46:29 -0300

    :sparkles: feat: quando um usuario da logout agora deleta o sector do armazenamento local

--- frontend/src/components/Layout/AppToolbar.jsx
+++ frontend/src/components/Layout/AppToolbar.jsx
@@ -29,17 +29,13 @@ const drawerWidth = 240;
 const AppToolbar = styled(MuiAppBar, {
     shouldForwardProp: (prop) => prop !== "open",
 })(({theme, open}) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(["width", "margin"], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
+    zIndex: theme.zIndex.drawer + 1, transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp, duration: theme.transitions.duration.leavingScreen,
+    }), ...(open && {
         marginLeft: drawerWidth,
         width: `calc(100% - ${drawerWidth}px)`,
         transition: theme.transitions.create(["width", "margin"], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
+            easing: theme.transitions.easing.sharp, duration: theme.transitions.duration.enteringScreen,
         }),
     }),
 }));
@@ -48,7 +44,7 @@ const CustomAppBar = ({open, toggleDrawer, is_admin}) => {
     const navigate = useNavigate();
     const {themeMode, toggleTheme} = useTheme();
     const {logout, user, loadingUser} = useAuth();
-    const {sector, setSector } = useStateContext();
+    const {sector, setSector} = useStateContext();
     const [sectors, setSectors] = useState([]);
     const location = useLocation();
     const pathNames = {
@@ -88,6 +84,7 @@ const CustomAppBar = ({open, toggleDrawer, is_admin}) => {
     const onLogout = async (ev) => {
         ev.preventDefault();
         await logout();
+        setSector(null);
         navigate("/login");
     };
 
@@ -97,8 +94,7 @@ const CustomAppBar = ({open, toggleDrawer, is_admin}) => {
         setSector(newSector);
     };
 
-    return (
-        <AppToolbar position="absolute" open={open}>
+    return (<AppToolbar position="absolute" open={open}>
             <Toolbar sx={{pr: "24px"}}>
                 <IconButton
                     edge="start"
@@ -106,8 +102,7 @@ const CustomAppBar = ({open, toggleDrawer, is_admin}) => {
                     aria-label="open drawer"
                     onClick={toggleDrawer}
                     sx={{
-                        marginRight: "36px",
-                        ...(open && {display: "none"}),
+                        marginRight: "36px", ...(open && {display: "none"}),
                     }}
                 >
                     <MenuIcon/>
@@ -125,45 +120,35 @@ const CustomAppBar = ({open, toggleDrawer, is_admin}) => {
                     <InputLabel
                         id="demo-simple-select-label"
                         sx={{
-                            color: "white",
-                            "&.Mui-focused": {
+                            color: "white", "&.Mui-focused": {
                                 color: "white",
                             },
                         }}
                     >
                         Setor
                     </InputLabel>
-                    {is_admin === false && (
-                        <Select
+                    {is_admin === false && (<Select
                             labelId="demo-customized-select-label"
                             id="demo-customized-select"
                             value={sector}
                             onChange={handleSectorChange}
                             input={<OutlinedInput label="Setor"/>}
                             sx={{
-                                color: "white",
-                                minWidth: 150,
-                                "& .MuiOutlinedInput-notchedOutline": {
+                                color: "white", minWidth: 150, "& .MuiOutlinedInput-notchedOutline": {
                                     borderColor: "white",
-                                },
-                                "&:hover .MuiOutlinedInput-notchedOutline": {
+                                }, "&:hover .MuiOutlinedInput-notchedOutline": {
                                     borderColor: "white",
-                                },
-                                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                                }, "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
                                     borderColor: "white",
-                                },
-                                "& .MuiSelect-icon": {
+                                }, "& .MuiSelect-icon": {
                                     color: "white",
                                 },
                             }}
                         >
-                            {sectors.map((sector, index) => (
-                                <MenuItem key={index} value={sector.sector_id}>
+                            {sectors.map((sector, index) => (<MenuItem key={index} value={sector.sector_id}>
                                     {sector.name}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    )}
+                                </MenuItem>))}
+                        </Select>)}
                 </FormControl>
                 <IconButton color="inherit" onClick={toggleTheme} sx={{m: 2}}>
                     {themeMode === "light" ? <Brightness4Icon/> : <Brightness7Icon/>}
@@ -172,8 +157,7 @@ const CustomAppBar = ({open, toggleDrawer, is_admin}) => {
                     <LogoutIcon/>
                 </IconButton>
             </Toolbar>
-        </AppToolbar>
-    );
+        </AppToolbar>);
 };
 
 export default CustomAppBar;
