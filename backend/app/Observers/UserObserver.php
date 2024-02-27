574886f0af4511d6818ab93507c36b47770038ca
luanaFOliveira <luluoliveira7@gmail.com>
2024-02-27 14:42:46 -0300

    :bug: fix: User observer quando deleta usuario

--- backend/app/Observers/UserObserver.php
+++ backend/app/Observers/UserObserver.php
@@ -3,26 +3,38 @@
 namespace App\Observers;
 
 use App\Models\Equipment;
+use App\Models\EquipmentRequest;
 use App\Models\Sector;
 use App\Models\User;
 use App\Models\UserEquipment;
 use App\Models\UserSector;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Query\JoinClause;
 
 class UserObserver
 {
     public function deleted(User $user)
     {
-        UserSector::where('user_id', $user->user_id)->delete();
         /*
         $equipmentsId = UserEquipment::where('user_id', $user->user_id)->pluck('equipment_id');
         foreach ($equipmentsId as $equipmentId) {
             Equipment::where('equipment_id', $equipmentId)->update(['is_available' => true]);
         }
         */
+        $user_id = $user->user_id;
+        
+        DB::table('equipment')
+            ->join('user_equipment', function (JoinClause $join) use ($user_id) {
+                $join->on('equipment.equipment_id', '=', 'user_equipment.equipment_id')
+                     ->where('user_equipment.user_id', $user_id);
+            })
+            ->update(['equipment.is_available' => true]);
 
-        UserEquipment::where('user_id', $user->user_id)
-                    ->join('equipment', 'user_equipment.equipment_id', '=', 'equipment.equipment_id')
-                    ->update(['is_available' => true]);
+        UserEquipment::where('user_id', $user_id)->update(['returned_at' => now()]);
+        UserEquipment::where('user_id', $user_id)->delete();
+        EquipmentRequest::where('user_id', $user_id)->update(['returned_at' => now()]);
+        EquipmentRequest::where('user_id', $user_id)->delete();
+        UserSector::where('user_id', $user->user_id)->delete();
 
     }
 }
